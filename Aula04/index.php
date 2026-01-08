476b0cc285ca64a3aed60e33bd7f4d3b91a13915
Gustavo Dias <lima364551441@outlook.com>
2026-01-08 18:01:18 -0300

    Aula04 - Instruções e ;

---
+++ Aula04/index.php
@@ -0,0 +1,11 @@
+<?php
+//Instrução e ponto e vírgula
+//Instrução: "falar" pro php fazer algo.
+$name = 'Gustavo'; //Sempre no final da instrução colocar ";' ou terá erro de sintaxe
+
+if($name === 'Gustavo'){
+    echo 'é Gustavo';
+}
+
+
+?>
